6311a5c3a89d7e5b28e9e621c591b1d0be57ffcb
toshlewi <adelewigitz@gmail.com>
2026-06-10 23:46:18 +0300

    updates

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -3,7 +3,7 @@ import CartContents from '../Cart/CartContents'
 
 const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
   return (
-  <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[25rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-5o ${drawerOpen ? "translate-x-o" : "translate-x-full" 
+  <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[25rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full" 
   }`}>
 
     {/*Close Button*/}
